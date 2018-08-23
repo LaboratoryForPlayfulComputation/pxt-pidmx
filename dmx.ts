0d4afa33ba96e3b197d41595869327921aed8881
shadowfax6894 <anniekelly6894@gmail.com>
2018-08-23 16:43:04 -0600

    added fixture block

--- dmx.ts
+++ dmx.ts
@@ -1,3 +1,20 @@
 //% color=#ab86f9 weight=5 icon="\uf140"
 namespace dmx {
+
+    let numberFixtures = 0;
+
+    /**
+     * Init new fixture
+     * @param numChannels
+     */
+    //% weight=90
+    //% blockId=dmx_initfixture block="fixture with %numChannels| channels" blockGap=8
+    export function fixture(numChannels: number): string {
+        let initcommand = "addFixture:" + numChannels.toString();
+        serial.writeLine(initcommand);
+        let fixtureName = "fixture:" + numberFixtures.toString();
+        numberFixtures++;
+        return fixtureName;
+    }
+
 }
