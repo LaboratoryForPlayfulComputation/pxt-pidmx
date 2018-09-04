8be097ae02c8a5e73b2334119591b83d266f7b4f
shadowfax6894 <anniekelly6894@gmail.com>
2018-09-04 15:46:12 -0600

    removed deprecated blocks

--- dmx.ts
+++ dmx.ts
@@ -4,43 +4,6 @@ namespace dmx {
     let numberFixtures = 0;
     let fixtures : Array<Fixture>= [];
 
-    /**
-     * Initialize a new fixture in the DMX universe
-     * @param numChannels
-     */
-    //% weight=90
-    //% blockId=dmx_initfixture block="fixture with %numChannels| channels" blockGap=8
-    export function fixture(numChannels: number): string {
-        let initcommand = "addFixture:" + numChannels.toString();
-        serial.writeLine(initcommand);
-        let fixtureName = numberFixtures.toString();
-        numberFixtures++;
-        return fixtureName;
-    }
-
-    /**
-     * Update fixture channel's value
-     * @param fixture
-     * @param channel
-     * @param value
-     */
-    //% weight=90
-    //% blockId=dmx_updatechannel block="%fixture| update channel %channel| to %value" blockGap=8
-    export function updateChannel(fixture: string, channel: number, value: number): void {
-        let updatecommand = "setChannelValue:" + fixture + "," + channel.toString() + "," + value.toString();
-        serial.writeLine(updatecommand);
-    }
-
-
-    /**
-     * Send all channels to dmx controller
-     */
-    //% weight=90
-    //% blockId=dmx_send block="send dmx" blockGap=8
-    export function send(): void {
-        serial.writeLine("updateChannels");
-    }
-
     export class Fixture {
         public name : string;
         public numChannels : number;
@@ -139,6 +102,15 @@ namespace dmx {
             fixture.channels[fixture.blueChannel].value = blue;
             send();
         }        
+    }
+
+    /**
+     * Send all channels to dmx controller
+     */
+    //% weight=90
+    //% blockId=dmx_send block="send dmx" blockGap=8
+    export function send(): void {
+        serial.writeLine("updateChannels");
     }
     
     /**
