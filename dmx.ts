e93a98edd563a48e42ad701f0724e4a981608df7
shadowfax6894 <anniekelly6894@gmail.com>
2018-09-04 15:03:36 -0600

    adding better way to update color channels

--- dmx.ts
+++ dmx.ts
@@ -2,6 +2,7 @@
 namespace dmx {
 
     let numberFixtures = 0;
+    let fixtures : Array<Fixture>= [];
 
     /**
      * Initialize a new fixture in the DMX universe
@@ -30,6 +31,7 @@ namespace dmx {
         serial.writeLine(updatecommand);
     }
 
+
     /**
      * Send all channels to dmx controller
      */
@@ -39,5 +41,118 @@ namespace dmx {
         serial.writeLine("updateChannels");
     }
 
+    export class Fixture {
+        public name : string;
+        public numChannels : number;
+        public channels : Array<Channel>;
+        public redChannel : number;
+        public greenChannel : number;
+        public blueChannel : number;
+        public RGBChannelsSet : boolean;
+
+        constructor(fixtureName : string, numberChannels : number) {
+            this.name = fixtureName;
+            this.numChannels = numberChannels;
+            this.RGBChannelsSet = false;
+        }
+    }
+
+    export class Channel {
+        public value : number;
+
+        constructor() {
+            this.value = 0;
+        }
+    }
     
+    /**
+     * Initialize a new fixture in the DMX universe
+     * @param name
+     * @param numChannels
+     */
+    //% weight=90
+    //% blockId=dmx_createfixture block="create fixture %name| with %numChannels| channels" blockGap=8
+    export function createFixture(name: string, numChannels: number) {
+        let initcommand = "addFixture:" + name + "," + numChannels.toString();
+        serial.writeLine(initcommand);
+        let fixtureName = numberFixtures.toString();
+        fixtures.push(new Fixture(name, numChannels));
+        numberFixtures++;
+        return fixtureName;
+    }
+
+    /**
+     * Update value of a fixture's particular channel
+     * @param name
+     * @param channel
+     * @param value
+     */
+    //% weight=90
+    //% blockId=dmx_updatefixturechannel block="update fixture %name| channel %numChannels| to %value" blockGap=8
+    export function updateFixtureChannel(name: string, channel: number, value: number) {
+        let fixture = findFixtureByName(name);
+        if (fixture){
+            fixture.channels[channel].value = value;
+            let updatecommand = "setChannelValue:" + name + "," + channel.toString() + "," + value.toString();
+            serial.writeLine(updatecommand);
+        }
+    }
+
+    /**
+     * Set which channels are RGB channels for the fixture
+     * @param name
+     * @param redChannel
+     * @param greenChannel
+     * @param blueChannel
+     */
+    //% weight=90
+    //% blockId=dmx_setRGBchannels block="fixture %name|'s RGB channels are red: %redChannel| green: %greenChannel| blue: %blueChannel" blockGap=8
+    export function setRGBChannels(name: string, redChannel: number, greenChannel: number, blueChannel: number) {
+        let fixture = findFixtureByName(name);
+        if (fixture){
+            fixture.redChannel = redChannel;
+            fixture.greenChannel = greenChannel;
+            fixture.blueChannel = blueChannel;
+            fixture.RGBChannelsSet = true;
+        }        
+    }
+
+    /**
+     * Update color of fixture
+     * @param name
+     * @param color
+     */
+    //% weight=90
+    //% blockId=dmx_updateFixtureChannel block="fixture %name| set color to %color" blockGap=8
+    export function updateFixtureColor(name: string, color: number) {
+        let fixture = findFixtureByName(name);
+        if (fixture && fixture.RGBChannelsSet){
+            let rgbValues = hexToRgb(color);
+            let red = rgbValues[0];
+            let green = rgbValues[1];
+            let blue = rgbValues[2];
+            fixture.channels[fixture.redChannel].value = red;
+            fixture.channels[fixture.greenChannel].value = green;
+            fixture.channels[fixture.blueChannel].value = blue;
+            send();
+        }        
+    }
+    
+    export function hexToRgb(hex: number): Array<number> {
+        let r = (hex >> 16) & 255;
+        let g = (hex >> 8) & 255;
+        let b = hex & 255;
+        return [r, g, b];
+    }
+
+    export function findFixtureByName(name: string) : Fixture {
+        for (let i = 0; i < fixtures.length; i++){
+            let fixture = fixtures[i];
+            if (fixture.name == name){
+                return fixture;
+            }
+        }
+        return null;
+    }
+
 }
--- projects/Untitled-2/main.ts
+++ projects/Untitled-2/main.ts
@@ -1,118 +1,13 @@
-//% color=#0062dB weight=96 block="Proximity"
-namespace proximity {
-    /* For saving most recent info */
-    let lastNumber: number = -1;
-    let lastString: string = "";
-    let lastBuffer: Buffer = null;
-    let lastTime: number = -1;
-    let lastSerial: number = -1;
-    let lastSignal: number = -1;
-    let lastKnownInformation: Array<RemoteMicrobit> = [];
-    let msgCount: number = 0;
-
-    export class RemoteMicrobit {
-        public serial: number;
-        public lastReceivedNumber: number;
-        public lastReceivedString: string;
-        public lastReceivedBuffer: Buffer;
-        public lastReceivedTime: number;
-        public lastReceivedSignal: number;
-    }
-
-    export class Packet {
-        /**
-         * The number payload if a number was sent in this packet (via ``sendNumber()`` or ``sendValue()``)
-         * or 0 if this packet did not contain a number.
-         */
-        public receivedNumber: number;
-        /**
-         * The string payload if a string was sent in this packet (via ``sendString()`` or ``sendValue()``)
-         * or the empty string if this packet did not contain a string.
-         */
-        public receivedString: string;
-        /**
-         * The buffer payload if a buffer was sent in this packet
-         * or the empty buffer
-         */
-        public receivedBuffer: Buffer;
-        /**
-         * The system time of the sender of the packet at the time the packet was sent.
-         */
-        public time: number;
-        /**
-         * The serial number of the sender of the packet or 0 if the sender did not sent their serial number.
-         */
-        public serial: number;
-        /**
-         * The received signal strength indicator (RSSI) of the packet.
-         */
-        public signal: number;
-    }
-
-    /* Whenever a new packet is recevied, update the last known packet information */
-    radio.onDataReceived(() => {
-        radio.receiveNumber();
-        const packet = new Packet();
-        packet.receivedNumber = radio.receivedNumber();
-        packet.time = radio.receivedTime();
-        packet.serial = radio.receivedSerial();
-        packet.receivedString = radio.receivedString();
-        packet.receivedBuffer = radio.receivedBuffer();
-        packet.signal = radio.receivedSignalStrength();
-        lastSerial = packet.serial;
-        let microbitAlreadyAdded = false;
-        for (let i = 0; i < lastKnownInformation.length; i++) {
-            if (lastKnownInformation[i].serial == packet.serial) {
-                lastKnownInformation[i].lastReceivedSignal = packet.signal;
-                microbitAlreadyAdded = true;
-            }
-        }
-        if (!microbitAlreadyAdded) {
-            let microbit = new RemoteMicrobit();
-            microbit.serial = packet.serial;
-            microbit.lastReceivedSignal = packet.signal;
-            lastKnownInformation.push(microbit);
-        }
-        msgCount += 1;
-    });
-
-    /** 
-     * 
-    */
-    //% blockId=radio_other_signal block="signal of other microbit %serialNo" blockGap=8
-    export function signalStrengthOfRemoteMicrobit(serialNo: number): number {
-        for (let i = 0; i < lastKnownInformation.length; i++) {
-            if (lastKnownInformation[i].serial == serialNo) {
-                return lastKnownInformation[i].lastReceivedSignal;
-            }
-        }
-        return -1;
-    }
-
-    export function lastKnownInfo(): string {
-        for (let i = 0; i < lastKnownInformation.length; i++) {
-            if (lastKnownInformation[i].serial == -1598746869) {
-                return "yes";
-            }
-        }
-        return "no";
-    }
-
-    export function numMessagesRecvd(): string {
-        return msgCount.toString();
-    }
-
-    export function printLastSerial(): string {
-        return lastSerial.toString();
-    }
-}
-
-
-radio.setGroup(1)
+let light1 = ""
+light1 = dmx.fixture(8)
+serial.writeLine("setChannelValue:" + "0," + 3 + "," + 100)
+serial.writeLine("setChannelValue:" + "0," + 4 + "," + 255)
+serial.writeLine("updateChannels")
 basic.forever(() => {
-    basic.showNumber(proximity.signalStrengthOfRemoteMicrobit(-1598746869));
-    serial.writeLine(proximity.lastKnownInfo());
-    serial.writeLine(proximity.numMessagesRecvd());
-    serial.writeLine(proximity.printLastSerial());
+    serial.writeLine("setChannelValue:" + "0," + 6 + "," + 0)
+    serial.writeLine("updateChannels")
+    basic.pause(5)
+    serial.writeLine("setChannelValue:" + "0," + 6 + "," + 255)
+    serial.writeLine("updateChannels")
+    basic.pause(5)
 })
-
