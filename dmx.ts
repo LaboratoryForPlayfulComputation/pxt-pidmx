3286d2942cb7a65e41a35b2909cb6d88f348dfe7
shadowfax6894 <anniekelly6894@gmail.com>
2018-09-04 15:15:46 -0600

    created an rgb block

--- dmx.ts
+++ dmx.ts
@@ -138,6 +138,21 @@ namespace dmx {
         }        
     }
     
+    /**
+     * Converts red, green, blue channels into a RGB color
+     * @param red value of the red channel between 0 and 255. eg: 255
+     * @param green value of the green channel between 0 and 255. eg: 255
+     * @param blue value of the blue channel between 0 and 255. eg: 255
+     */
+    //% blockId="colors_rgb" block="red %red|green %green|blue %blue"
+    //% red.min=0 red.max=255 green.min=0 green.max=255 blue.min=0 blue.max=255
+    //% advanced=false    
+    //% weight=19
+    //% blockGap=8
+    export function rgb(red: number, green: number, blue: number): number {
+        return ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+    }
+
     export function hexToRgb(hex: number): Array<number> {
         let r = (hex >> 16) & 255;
         let g = (hex >> 8) & 255;
